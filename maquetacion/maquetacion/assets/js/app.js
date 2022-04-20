35b013e7f0702e1215034ce028f24446d5bc16b0
unknown <jenha04@gmail.com>
2022-04-19 22:13:43 -0500

    Creación del proyecto

---
+++ maquetacion/maquetacion/assets/js/app.js
@@ -0,0 +1,17 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    menuMobile();
+})
+
+
+function menuMobile() {
+    const menu = document.querySelector('.menu-mobile');
+    menu.addEventListener('click', function () {
+        const navegacion = document.querySelector('.navegacion')
+        if (navegacion.classList.contains('mostrar')) {
+            navegacion.classList.remove('mostrar');
+        } else {
+            navegacion.classList.add('mostrar');
+
+        }
+    })
+}
