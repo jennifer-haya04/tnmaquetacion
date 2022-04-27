49e9e95cd0ee2440d977b6b03338052f8a2de895
unknown <jenha04@gmail.com>
2022-04-26 20:25:29 -0500

    Proyecto finalizado de maquetacion

--- maquetacion/maquetacion/assets/js/app.js
+++ maquetacion/maquetacion/assets/js/app.js
@@ -7,11 +7,13 @@ function menuMobile() {
     const menu = document.querySelector('.menu-mobile');
     menu.addEventListener('click', function () {
         const navegacion = document.querySelector('.navegacion')
-        if (navegacion.classList.contains('mostrar')) {
+        /*if (navegacion.classList.contains('mostrar')) {
             navegacion.classList.remove('mostrar');
         } else {
             navegacion.classList.add('mostrar');
 
-        }
+        }*/
+
+        navegacion.classList.toggle('mostrar');
     })
 }
